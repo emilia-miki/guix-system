4acf3d999fbe5b3a72731c6fcf5e07440e7c68be
Emilia Miki <emiliamiki@icloud.com>
2026-06-15 00:38:54 +0300

    Add wofi style.css

--- home/sway.scm
+++ home/sway.scm
@@ -114,6 +114,66 @@
                       ,(executable-file "wifimenu" (local-file "files/waybar-wifimenu")))
                      ("waybar/wofi-bluetooth"
                       ,(executable-file "wofi-bluetooth" (local-file "files/waybar-wofi-bluetooth")))
+                     ("wofi/style.css"
+                      ,(plain-file "wofi-style.css"
+                                   "\
+window {
+    margin: 0px;
+    background-color: #191724;
+    border-radius: 0px;
+    border: 2px solid #eb6f92;
+    color: #e0def4;
+    font-family: 'FiraCode Nerd Font', 'Unifont Upper', 'Noto Sans Symbols 2', 'Noto Sans Symbols', 'Noto Sans';
+    font-size: 20px;
+}
+
+#input {
+    margin: 5px;
+    border-radius: 0px;
+    border: none;
+    color: #eb6f92;
+    background-color: #26233a;
+}
+
+#inner-box {
+    margin: 5px;
+    border: none;
+    background-color: #26233a;
+    color: #e0def4;
+    border-radius: 0px;
+}
+
+#outer-box {
+    margin: 15px;
+    border: none;
+    background-color: #191724;
+}
+
+#scroll {
+    margin: 0px;
+    border: none;
+}
+
+#text {
+    margin: 5px;
+    border: none;
+    color: #e0def4;
+}
+
+#entry:selected {
+    background-color: #eb6f92;
+    color: #191724;
+    border-radius: 0px;
+    outline: none;
+}
+
+#entry:selected * {
+    background-color: #eb6f92;
+    color: #191724;
+    border-radius: 0px;
+    outline: none;
+}
+"))
                      ("rofimoji.rc"
                       ,(plain-file "rofimoji.rc"
                                    "\
